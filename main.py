2bc1cddefa4b48351823f52cbfbf74d7c0ef9ef5
Nicolas <nicolasbaptista1309@gmail.com>
2025-06-21 13:44:03 -0300

    fix: corrigir erro de input no saque

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def menu():
 
 
 def main():
-    conta = criar_conta()  # Cria conta com valores padrão do config
+    conta = criar_conta()  
 
     while True:
         opcao = input(menu()).lower()
@@ -25,16 +25,13 @@ def main():
             except ValueError:
                 print("Operação falhou! Digite um número válido.")
 
-        # MUDANÇA SUGERIDA NO main.py PARA DEBUGAR
         elif opcao == "s":
             try:
                 valor = float(input("Informe o valor do saque: "))
                 sacar(conta, valor)
-            except ValueError as e: # Adicionamos "as e"
-                print("\n--- DEBUG ---")
-                print("Ocorreu um ValueError inesperado. Detalhes do erro:")
-                print(e) # Isso vai imprimir a mensagem de erro real do Python
-                print("--- FIM DO DEBUdG ---\n")
+            except ValueError:
+                print("Entrada inválida! Digite um número como 10 ou 10.50, sem letras.")
+
 
         elif opcao == "e":
             ver_extrato(conta)
